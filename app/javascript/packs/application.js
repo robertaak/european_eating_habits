dbbaa84e7b2f4d0dd24746cb4bfbe42479dd1518
Roberta <r.kimerale@gmail.com>
2022-11-02 21:53:27 +0200

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require chartkick
-//= require Chart.bundle
+ require("chartkick")
+ require("chart.js")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "chartkick/chart.js"
 
 Rails.start()
 Turbolinks.start()
